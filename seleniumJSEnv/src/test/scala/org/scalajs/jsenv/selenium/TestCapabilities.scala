b620547b70bb6f2548763a0225ba3239cb608b74
Tobias Schlatter <tobias@meisch.ch>
2018-07-21 11:43:52 +0200

    Use headless versions of browsers

--- seleniumJSEnv/src/test/scala/org/scalajs/jsenv/selenium/TestCapabilities.scala
+++ seleniumJSEnv/src/test/scala/org/scalajs/jsenv/selenium/TestCapabilities.scala
@@ -6,8 +6,8 @@ import org.openqa.selenium.chrome.ChromeOptions
 
 object TestCapabilities {
   def fromEnv: Capabilities = sys.env.getOrElse("SJS_TEST_BROWSER", "firefox") match {
-    case "firefox" => new FirefoxOptions
-    case "chrome"  => new ChromeOptions
+    case "firefox" => new FirefoxOptions().setHeadless(true)
+    case "chrome"  => new ChromeOptions().setHeadless(true)
     case name      => throw new IllegalArgumentException(s"Unknown browser $name")
   }
 }

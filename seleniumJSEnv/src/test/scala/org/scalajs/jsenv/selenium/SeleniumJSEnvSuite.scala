47ac5f02d68f9580bda25f0552b38892eb347caa
Tobias Schlatter <tobias@meisch.ch>
2021-01-20 13:20:47 +0100

    Fix #105: Re-enable largeMessageTest
    The upstream issue [1] has been fixed in Chrome 89, which has just hit
    the GitHub Actions virtual environment [2].
    
    [1]: https://bugs.chromium.org/p/chromedriver/issues/detail?id=3672
    [2]: https://github.com/actions/virtual-environments/releases/tag/ubuntu20%2F20210309.1

--- seleniumJSEnv/src/test/scala/org/scalajs/jsenv/selenium/SeleniumJSEnvSuite.scala
+++ seleniumJSEnv/src/test/scala/org/scalajs/jsenv/selenium/SeleniumJSEnvSuite.scala
@@ -10,28 +10,7 @@ import org.junit.runners.Suite
 import org.junit.runner.manipulation.Filter
 import org.junit.runner.Description
 
-@RunWith(classOf[SeleniumJSSuiteRunner])
+@RunWith(classOf[JSEnvSuiteRunner])
 class SeleniumJSSuite extends JSEnvSuite(
   JSEnvSuiteConfig(new SeleniumJSEnv(TestCapabilities.fromEnv))
 )
-
-class SeleniumJSSuiteRunner private (
-    root: Class[_], base: JSEnvSuiteRunner)
-    extends Suite(root, Arrays.asList[Runner](base)) {
-
-  /** Constructor for reflective instantiation via `@RunWith`. */
-  def this(suite: Class[_ <: SeleniumJSSuite]) =
-    this(suite, new JSEnvSuiteRunner(suite))
-
-  // Ignore `largeMessageTest` for chrome.
-  if (TestCapabilities.nameFromEnv == "chrome") {
-    base.filter(new Filter {
-      def describe(): String = "Ignore largeMessageTest"
-
-      def shouldRun(description: Description): Boolean = {
-        description.getMethodName == null ||
-        !description.getMethodName.startsWith("largeMessageTest")
-      }
-    })
-  }
-}
